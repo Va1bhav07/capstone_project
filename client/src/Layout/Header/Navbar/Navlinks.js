5017f6c9551a25403a9b22eea44eea7bec4382c4
Vaibhav Arora <vaibhavarora8401@gmail.com>
2024-03-05 11:07:19 -0500

    added saga and login is now transfered to saga

--- client/src/Layout/Header/Navbar/Navlinks.js
+++ client/src/Layout/Header/Navbar/Navlinks.js
@@ -3,8 +3,19 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutAction } from "../../../services/actions/authActions";
 
 function Navlinks() {
+  const dispatch = useDispatch();
+
+  const authData = useSelector((state) => state.authReducer);
+  const handleNavSelect = (eventKey) => {
+    if (eventKey === "logout") {
+      dispatch(logoutAction());
+    }
+  };
+
   return (
     <Navbar.Collapse id="basic-navbar-nav" className="">
       <Nav className="justify-content-center flex-grow-1">
@@ -21,9 +32,17 @@ function Navlinks() {
         />
         <Button variant="outline-success">Search</Button>
       </Form> */}
-      <Nav>
-        <Nav.Link href="/login">Login</Nav.Link>
-        <Nav.Link href="/signup">Sign Up</Nav.Link>
+      <Nav onSelect={handleNavSelect}>
+        {!authData.isLoggedIn ? (
+          <>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/signup">Sign Up</Nav.Link>
+          </>
+        ) : (
+          <>
+            <Nav.Link eventKey="logout">Logout</Nav.Link>
+          </>
+        )}
       </Nav>
     </Navbar.Collapse>
   );
