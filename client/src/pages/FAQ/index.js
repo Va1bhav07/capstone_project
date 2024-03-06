b3b8f790356c2882069488a283cb59fb99b23c5a
Vaibhav Arora <vaibhavarora8401@gmail.com>
2024-03-05 20:14:35 -0500

    fixed style for about and faq

--- client/src/pages/FAQ/index.js
+++ client/src/pages/FAQ/index.js
@@ -1,65 +1,46 @@
 import React from "react";
-// import Header from './Header'; // Import Header component
-// import Footer from "./Footer"; // Import Footer component
-import "./FAQs.css"; // Import CSS for styling
+import Accordion from "react-bootstrap/Accordion";
 
 const FAQs = () => {
   return (
-    <div className="full-page">
-      {/* <Header /> */}
-      <div className="faqs-container">
-        <div className="faqs">
-          <h1 className="faqs-heading">Frequently Asked Questions</h1>
-          <div className="faq">
-            <input type="checkbox" className="faq-toggle" id="faq1" />
-            <label className="faq-question" htmlFor="faq1">
-              What is EduFlex?
-            </label>
-            <div className="faq-answer">
-              EduFlex is an innovative online learning platform designed to
-              provide users with a comprehensive and flexible learning
-              experience. It offers a diverse range of courses spanning various
-              subjects, including technology, business, arts, sciences, and
-              more. EduFlex aims to empower learners of all ages and backgrounds
-              to acquire new skills, expand their knowledge, and achieve their
-              personal and professional goals.
-            </div>
-          </div>
-          <div className="faq">
-            <input type="checkbox" className="faq-toggle" id="faq2" />
-            <label className="faq-question" htmlFor="faq2">
-              How do I enroll in a course?
-            </label>
-            <div className="faq-answer">
-              To enroll in a course, simply navigate to the course page and
-              click on the "Enroll" button. Follow the prompts to complete the
-              enrollment process.
-            </div>
-          </div>
-          <div className="faq">
-            <input type="checkbox" className="faq-toggle" id="faq3" />
-            <label className="faq-question" htmlFor="faq3">
-              Can I access course materials offline?
-            </label>
-            <div className="faq-answer">
-              Yes, you can download course materials for offline access. Look
-              for the download option next to each resource.
-            </div>
-          </div>
-          <div className="faq">
-            <input type="checkbox" className="faq-toggle" id="faq4" />
-            <label className="faq-question" htmlFor="faq4">
-              How do I contact support?
-            </label>
-            <div className="faq-answer">
-              You can contact support by emailing support@eduflex.com or by
-              filling out the contact form on our website.
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <Footer /> */}
-    </div>
+    <Accordion defaultActiveKey="0" className="mt-4">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header> What is EduFlex?</Accordion.Header>
+        <Accordion.Body>
+          EduFlex is an innovative online learning platform designed to provide
+          users with a comprehensive and flexible learning experience. It offers
+          a diverse range of courses spanning various subjects, including
+          technology, business, arts, sciences, and more. EduFlex aims to
+          empower learners of all ages and backgrounds to acquire new skills,
+          expand their knowledge, and achieve their personal and professional
+          goals.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header> How do I enroll in a course?</Accordion.Header>
+        <Accordion.Body>
+          To enroll in a course, simply navigate to the course page and click on
+          the "Enroll" button. Follow the prompts to complete the enrollment
+          process.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>
+          Can I access course materials offline?
+        </Accordion.Header>
+        <Accordion.Body>
+          Yes, you can download course materials for offline access. Look for
+          the download option next to each resource.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="3">
+        <Accordion.Header> How do I contact support?</Accordion.Header>
+        <Accordion.Body>
+          You can contact support by emailing support@eduflex.com or by filling
+          out the contact form on our website.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 };
 
